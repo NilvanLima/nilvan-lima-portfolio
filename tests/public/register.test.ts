243298981081181361356d7042b6ea009fcdb0df
NilvanLima <nilvanga@gmail.com>
2026-09-07 23:21:19 -0300

    add register test

---
+++ tests/public/register.test.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+import { randomEmail } from "../fixtures/test-data";
+
+test.describe("Register feature", () => {
+    test("should register successfully", async ({ page }) => {
+        await page.goto("/register");
+
+        await page.getByTestId("register-name").fill("test-name");
+        await page.getByTestId("register-email").fill(randomEmail());
+        await page.getByTestId("register-password").fill("test1234");
+        await page.getByTestId("register-submit").click();
+
+        await expect(page).toHaveURL("/");
+        await expect(page.getByTestId("toast-message")).toBeVisible();
+        await expect(page.getByTestId("toast-message")).toHaveText("Conta criada com sucesso");
+    });
+});
